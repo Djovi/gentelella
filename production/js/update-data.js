51faa6605b2c1add76657a93ab4be6916136d7fa
Joel Hounakey <aandtandplus@gmail.com>
2024-03-07 10:43:03 +0000

    Update update-data.js

--- production/js/update-data.js
+++ production/js/update-data.js
@@ -3,7 +3,17 @@ const apiToken = "YOUR_API_TOKEN";
 
 // Replace with the specific ClickUp API endpoint URL
 const apiUrl = "https://api.clickup.com/v2/list/LIST_ID";
-
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCgxVSDFQsS0Yi79vD8T65TgV3oDQt9sB4",
+  authDomain: "magrotte-a4cf4.firebaseapp.com",
+  databaseURL: "https://magrotte-a4cf4.firebaseio.com",
+  projectId: "magrotte-a4cf4",
+  storageBucket: "magrotte-a4cf4.appspot.com",
+  messagingSenderId: "882468575422",
+  appId: "1:882468575422:web:8aec1d8a4dbd1e756926ef",
+  measurementId: "G-RSFV6Y8KCB"
+};
 // Function to fetch data from ClickUp API
 async function fetchData() {
   try {
