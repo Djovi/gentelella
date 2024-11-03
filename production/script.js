c6d98b345dc2f7d9bb0a1f8cbf308752a77553a7
Joel Hounakey <aandtandplus@gmail.com>
2024-11-03 04:59:20 -0800

    Update script.js

--- production/script.js
+++ production/script.js
@@ -1,15 +1,15 @@
 const url = "https://instantapi.ai/api/retrieve/";
 
 const data = {
- //   / "webpage_url": "https://tg.coinafrique.com/profil/c4c18891-6b02-4cf2-881c-6de09703ec3f?page=2",
-   "webpage_url": "https://tg.coinafrique.com/annonce/appartements/location-appartement-attilamonou-4930530",
-//  "api_method_name": "getAllItems",
- //  "api_response_structure": "{\"Items\":[{\"name\":\"<the full title of the item, including any hashtags and emojis>\",\"price\":\"<the defined price of the item>\",\"author\":\"<the publisher of the item>\",\"item_url\":\"<the absolute URL to the item details>\",\"item_image\":\"<the absolute URL to the item image>\"}]}",
+  "webpage_url": "https://tg.coinafrique.com/profil/c4c18891-6b02-4cf2-881c-6de09703ec3f?page=2",
+ //  "webpage_url": "https://tg.coinafrique.com/annonce/appartements/location-appartement-attilamonou-4930530",
+   "api_method_name": "getProductsList",
+   "api_response_structure": "{\"Items\":[{\"name\":\"<the full title of the item, including any hashtags and emojis>\",\"price\":\"<the defined price of the item>\",\"author\":\"<the publisher of the item>\",\"item_url\":\"<the absolute URL to the item details>\",\"item_image\":\"<the absolute URL to the item image>\"}]}",
   
  //  "webpage_url": "https://instantapi.ai/",
-    "api_method_name": "getProductInformation",
-   "api_response_structure": "{\"product_name\":\"\",\"product_description\":\"\",\"product_use_cases\":\"\",\"copyright_company_name\":\"\",\"tax_registration_number\":\"\"}",
-    "api_key": "free_joel@rechainglobal.com"
+  //  "api_method_name": "getProductInformation",
+  // "api_response_structure": "{\"product_name\":\"\",\"product_description\":\"\",\"product_use_cases\":\"\",\"copyright_company_name\":\"\",\"tax_registration_number\":\"\"}",
+   "api_key": "free_joel@rechainglobal.com"
 };
 
 // Display a loading message while waiting for the API response
@@ -27,7 +27,7 @@ fetch(url, {
 .then(data => {
     console.log(data);
     // Replace loading message with JSON data in a readable format
-    document.getElementById("responseContainer").textContent = JSON.stringify(data, null, 2);
+    document.getElementById("responseContainer").textContent = JSON.stringify(data.Items, null, 2);
 })
 .catch(error => {
     console.error('Error:', error);
