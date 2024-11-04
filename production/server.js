ff6b846eb29038557741760242ed63003859e1f8
Joel Hounakey <aandtandplus@gmail.com>
2024-11-04 04:24:40 -0800

    Update server.js

--- production/server.js
+++ production/server.js
@@ -1,1 +1,44 @@
 //pk_live_fgMxvyvoSrhEGCMCNixTpD7R
+// server.js
+//
+// Use this sample code to handle webhook events in your integration.
+//
+// 1) Paste this code into a new file (server.js)
+//
+// 2) Install dependencies
+//   npm install stripe
+//   npm install express
+//
+// 3) Run the server on http://localhost:4242
+//   node server.js
+
+// The library needs to be configured with your account's secret key.
+// Ensure the key is kept out of any version control system you might be using.
+const stripe = require('stripe')('sk_test_...');
+const express = require('express');
+const app = express();
+
+
+// This is your Stripe CLI webhook secret for testing your endpoint locally.
+const endpointSecret = "whsec_39ed95c0f9a7efecd567b9450b2adc01727944494590bbfe22be5a28833a0ed7";
+
+app.post('/webhook', express.raw({type: 'application/json'}), (request, response) => {
+  const sig = request.headers['stripe-signature'];
+
+  let event;
+
+  try {
+    event = stripe.webhooks.constructEvent(request.body, sig, endpointSecret);
+  } catch (err) {
+    response.status(400).send(`Webhook Error: ${err.message}`);
+    return;
+  }
+
+  // Handle the event
+  console.log(`Unhandled event type ${event.type}`);
+
+  // Return a 200 response to acknowledge receipt of the event
+  response.send();
+});
+
+app.listen(4242, () => console.log('Running on port 4242'));
