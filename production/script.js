c8e60aadc3a919c1c2a39f2dcfa2d646a1d6ed1b
Joel Hounakey <aandtandplus@gmail.com>
2024-11-11 07:56:01 -0800

    Update script.js

--- production/script.js
+++ production/script.js
@@ -20,11 +20,11 @@ const data = {
 // Display a loading message while waiting for the API response
 document.getElementById("responseContainer").innerHTML = '<span class="loading">Loading<span class="cursor">|</span></span>';
 const jsonData = {
-       "title": 'Location boutique - Kodjoviakope',
-      "price": '120 000',
-      "author": 'Samuel',
-      "product_url": 'https://tg.coinafrique.com/annonce/bureaux-et-commerces/location-boutique-kodjoviakope-4924416',
-      "item_image": 'https://images.coinafrique.com/thumb_4924416_uploaded_image1_1725345786.jpg'
+       title: 'Location boutique - Kodjoviakope',
+      price: '120 000',
+      author: 'Samuel',
+       product_url: 'https://tg.coinafrique.com/annonce/bureaux-et-commerces/location-boutique-kodjoviakope-4924416',
+      item_image: 'https://images.coinafrique.com/thumb_4924416_uploaded_image1_1725345786.jpg'
 };
 
    fetch(scriptURL, { 
