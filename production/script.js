f47132f84227779d84358f3430f92a17a0d586b2
Joel Hounakey <aandtandplus@gmail.com>
2024-12-17 19:51:08 -0800

    Update script.js

--- production/script.js
+++ production/script.js
@@ -1,4 +1,4 @@
-/** const url = "https://instantapi.ai/api/retrieve/";
+ const url = "https://instantapi.ai/api/retrieve/";
  const scriptURL = 'https://script.google.com/macros/s/AKfycbx6sNcLlKSg8Ysc8pgp_UQQm8Z4d2nXzJoUaOpqFC-e77s1kQHnzbvwmcf_woT9ecDv/exec'
                      
 
@@ -6,12 +6,12 @@
      
    
  const mdata = {
-  "webpage_url": "https://tg.coinafrique.com/profil/c4c18891-6b02-4cf2-881c-6de09703ec3f?page=2",
+ // "webpage_url": "https://tg.coinafrique.com/profil/c4c18891-6b02-4cf2-881c-6de09703ec3f?page=2",
  //  "webpage_url": "https://tg.coinafrique.com/annonce/appartements/location-appartement-attilamonou-4930530",
    "api_method_name": "getProductsList",
    "api_response_structure": "{\"Items\":[{\"name\":\"<the full title of the item, including any hashtags and emojis>\",\"price\":\"<the defined price of the item>\",\"author\":\"<the publisher of the item>\",\"item_url\":\"<the absolute URL to the item details>\",\"item_image\":\"<the absolute URL to the item image>\"}]}",
   
-  // "webpage_url": "https://www.amazon.com/Schneider-Electric-HOM120PDFC-Homeline-Single-Pole/dp/B07TW6LKJZ/ref=sr_1_5?crid=GO5AGC9SDDB7&dib=eyJ2IjoiMSJ9.uE4deo7r5VmfAYy0VaoW5gZ-srtMCvhZPBYVlq8YKWUPWVIIM4exQQ0LpVlh4XthMBWh4WCIOYbrI4pXeyS44NYRuAl9wuMj_KlZe6GTmrSeUUuSM8ITD4af3PLZz4jEI4-izPm_70FLSvoN57VxbWx_qW7UFr8RbF2ih0Zo49o1hs69ZI6sDGje2R56pczekwEah2t2qky1y3H6TceejUWrxHRTNBM2ljjtmUNsNvU.sCn4ekKGnzaVXB6k-c1nidwTJUXgOsbXWhnxD0LG8F8&dib_tag=se&keywords=Schneider+Electric&psr=EY17&qid=1730050709&s=todays-deals&sprefix=schneider+electric+%2Ctodays-deals%2C740&sr=1-5-catcorr",
+   "webpage_url": "https://www.amazon.com/Schneider-Electric-HOM120PDFC-Homeline-Single-Pole/dp/B07TW6LKJZ/ref=sr_1_5?crid=GO5AGC9SDDB7&dib=eyJ2IjoiMSJ9.uE4deo7r5VmfAYy0VaoW5gZ-srtMCvhZPBYVlq8YKWUPWVIIM4exQQ0LpVlh4XthMBWh4WCIOYbrI4pXeyS44NYRuAl9wuMj_KlZe6GTmrSeUUuSM8ITD4af3PLZz4jEI4-izPm_70FLSvoN57VxbWx_qW7UFr8RbF2ih0Zo49o1hs69ZI6sDGje2R56pczekwEah2t2qky1y3H6TceejUWrxHRTNBM2ljjtmUNsNvU.sCn4ekKGnzaVXB6k-c1nidwTJUXgOsbXWhnxD0LG8F8&dib_tag=se&keywords=Schneider+Electric&psr=EY17&qid=1730050709&s=todays-deals&sprefix=schneider+electric+%2Ctodays-deals%2C740&sr=1-5-catcorr",
   // "api_method_name": "getProductInformation",
   // "api_response_structure": "{\"product_name\":\"\",\"product_description\":\"\",\"product_use_cases\":\"\",\"copyright_company_name\":\"\",\"tax_registration_number\":\"\"}",
   "api_key": "free_joel@rechainglobal.com"
@@ -67,34 +67,5 @@ document.getElementById("responseContainer").innerHTML = '<span class="loading">
 .catch(error => {
     console.error('Error:', error);
     document.getElementById("responseContainer").textContent = `Error: ${error.message}`;
-});**/
-const url = "https://instantapi.ai/api/retrieve/";
-
-const data = {
-    "webpage_url": "https://instantapi.ai/",
-    "api_method_name": "getProductInformation",
-    "api_response_structure": "{\"product_name\":\"\",\"product_description\":\"\",\"product_use_cases\":\"\",\"copyright_company_name\":\"\",\"tax_registration_number\":\"\"}",
-    "api_key": "free_joel@rechainglobal.com"
-};
-
-// Display a loading message while waiting for the API response
-document.getElementById("responseContainer").innerHTML = '<span class="loading">Loading<span class="cursor">|</span></span>';
-
-// Function to fetch data and display it in the responseContainer div
-fetch(url, {
-    method: 'POST',
-    headers: {
-        'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(data)
-})
-.then(response => response.json())
-.then(data => {
-    console.log(data);
-    // Replace loading message with JSON data in a readable format
-    document.getElementById("responseContainer").textContent = JSON.stringify(data, null, 2);
-})
-.catch(error => {
-    console.error('Error:', error);
-    document.getElementById("responseContainer").textContent = `Error: ${error.message}`;
-});
+}); 
+ 
