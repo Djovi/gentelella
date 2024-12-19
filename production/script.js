43702e72b1f17cdd84131456237c351a99a61260
Joel Hounakey <aandtandplus@gmail.com>
2024-12-19 10:11:29 -0800

    Update script.js

--- production/script.js
+++ production/script.js
@@ -46,7 +46,7 @@ fetch(url, {
 })
 .then(dataFromFirstFetch => {
     // Now send the data to your Apps Script
-      responseContainer.textContent = JSON.stringify(dataFromFirstFetch, null, 2);
+    //  responseContainer.textContent = JSON.stringify(dataFromFirstFetch, null, 2);
    
      // Call the render function with data
         renderProducts(dataFromFirstFetch);
@@ -63,7 +63,7 @@ fetch(url, {
                 
         // Add error handling for image loading
         img.onerror = () => {
-            img.src = 'https://m.media-amazon.com/images/I/41u2CjtkyOL.jpg'; // Fallback image URL
+            img.src = product.item_image; // Fallback image URL
             console.error(`Failed to load image: ${product.image_url}`);
         };
                 const name = document.createElement('div');
