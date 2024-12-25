2a188d722bbbe8ca3c7dc3da99e386877bf086c0
Joel Hounakey <aandtandplus@gmail.com>
2024-12-25 11:37:41 -0800

    Update script.js

--- production/script.js
+++ production/script.js
@@ -130,7 +130,7 @@ fetch(url, {
                 
                 const price = document.createElement('div');
                 price.className = 'price';
-                price.textContent ='CFA '+(((parseFloat(product.price.replace('₦', ''))*100))+(((parseFloat(product.price.replace('₦', ''))*100))*0.40))*0.41 //product.price;
+                price.textContent ='CFA '+(((parseFloat(product.price.replace('₦', ''))*100))+(((parseFloat(product.price.replace('₦', ''))*100))*0.40))*4.1 //product.price;
                 
                 productItem.appendChild(img);
                 productItem.appendChild(name);
